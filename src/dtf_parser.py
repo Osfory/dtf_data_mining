0fc0fef5215191839a8450187b154b6d97f3e3d7
Aleksey Belyantsev <belyantsev.aleksey@gmail.com>
2021-02-07 17:12:12 +0300

    Some fixes

--- src/dtf_parser.py
+++ src/dtf_parser.py
@@ -1,9 +1,8 @@
 from pathlib import Path
-import requests
-
 import json
-import csv
+import requests
 import pandas as pd
+import csv
 
 
 class ConfigLoader:
@@ -16,14 +15,10 @@ class ConfigLoader:
 
 config = ConfigLoader.load()
 
-# user_agent = 'Chromium/88.0.4324.146 (PC; Ubuntu/20.04.2 LTS; en-US; 1600x900)'
-# headers = {'User-Agent': user_agent, "X-Device-Token": config['api']['token']}
-#
+# headers = {'User-Agent': config['user_agent'], "X-Device-Token": config['api']['token']}
+
 # api_url = 'https://api.dtf.ru/v1.8/subsite/261696'
 #
 # response = requests.get(api_url, params = headers)
 # print(response.status_code)
 # print(response.json())
-
-
-
