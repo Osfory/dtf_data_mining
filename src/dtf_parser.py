f9fac89bdc44bdee3c2b0a20c34198544c1909b1
Aleksey Belyantsev <belyantsev.aleksey@gmail.com>
2021-02-07 16:46:51 +0300

    init

--- src/dtf_parser.py
+++ src/dtf_parser.py
@@ -1,22 +1,29 @@
+from pathlib import Path
 import requests
-import urllib.request
+
 import json
 import csv
-
 import pandas as pd
 
-import time
-import functional
 
-api_url = 'https://api.dtf.ru/v1.8/subsite/261696'
+class ConfigLoader:
+    @staticmethod
+    def load():
+        script_path = Path(__file__).parents[1].absolute()
+        with open(str(script_path) + "/config.json") as json_file:
+            return json.load(json_file)
 
-user_agent = 'Chromium/88.0.4324.146 (PC; Ubuntu/20.04.2 LTS; en-US; 1600x900)'
-headers = {'User-Agent': user_agent}
 
-response = requests.get(api_url, params = headers)
-print(response.status_code)
-print(response.json())
-# print(res.headers['Content-Type'])
-# data = response.json()
+config = ConfigLoader.load()
+
+# user_agent = 'Chromium/88.0.4324.146 (PC; Ubuntu/20.04.2 LTS; en-US; 1600x900)'
+# headers = {'User-Agent': user_agent, "X-Device-Token": config['api']['token']}
+#
+# api_url = 'https://api.dtf.ru/v1.8/subsite/261696'
+#
+# response = requests.get(api_url, params = headers)
+# print(response.status_code)
+# print(response.json())
+
 
 
