77dde874c5678031cbc4087f77da3d22549177e7
Aleksey Belyantsev <belyantsev.aleksey@gmail.com>
2021-03-04 21:14:35 +0300

    Script cleaned up

--- src/dtf_parser.py
+++ src/dtf_parser.py
@@ -1,24 +1,41 @@
-from pathlib import Path
 import json
 import requests
+import urllib.request
 import pandas as pd
-import csv
+import time
 
 
 class ConfigLoader:
     @staticmethod
     def load():
-        script_path = Path(__file__).parents[1].absolute()
+        script_path = "YOUR_PATH" # "YOUR_PATH"
         with open(str(script_path) + "/config.json") as json_file:
             return json.load(json_file)
 
 
 config = ConfigLoader.load()
+headers = {"User-Agent": config['user_agent']} #, "X-Device-Token": config['token']}
+api_url = 'https://api.dtf.ru/v1.8/subsite/261696/timeline/new?count=50'
 
-# headers = {'User-Agent': config['user_agent'], "X-Device-Token": config['api']['token']}
+request = urllib.request.Request(api_url, None, headers)
+response = urllib.request.urlopen(request)
+data = response.read()
+entry = json.loads(data)
 
-# api_url = 'https://api.dtf.ru/v1.8/subsite/261696'
-#
-# response = requests.get(api_url, params = headers)
-# print(response.status_code)
-# print(response.json())
+df = pd.DataFrame()
+
+for n in range(150):
+    print(api_url + '&offset=' + str(50 * n))
+    try:
+        response = requests.get((api_url + '&offset=' + str(50 * n)), params=headers)
+    except Exception as e:
+        print(e)
+        if 'Too many requests' in e:
+            print('Слишком много запросов, дружок-пирожок. Отдохни немного. \nНомер страницы = {}'.format(n))
+            time.sleep(30)
+    else:
+        df = pd.concat([df, pd.DataFrame(response.json()['result'])], axis=0)
+
+print(df)
+print(df.shape)
+df.to_csv("YOUR_ANOTHER_PATH")
